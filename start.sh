2ed7405e8435148f68047598af3f36307c9f0aaa
Devyn Cairns <devyn.cairns@gmail.com>
2013-01-09 14:46:20 -0800

    Add shebang to start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # Your assigned port range is 39410-39419, and you may use these for anything you want.
 # Your HTTP server should use port 39410. If you want to start more processes, just let me know.
 
