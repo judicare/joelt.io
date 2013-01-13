5b4af142eea4078ecddbc0a547fdb32c7d974f3c
Devyn Cairns <devyn.cairns@gmail.com>
2013-01-12 21:09:36 -0800

    remove umask, add update

--- start.sh
+++ start.sh
@@ -2,7 +2,9 @@
 # Your assigned port range is 39410-39419, and you may use these for anything you want.
 # Your HTTP server should use port 39410. If you want to start more processes, just let me know.
 
-umask 007
+# We need to check out your source code.
+cd /mnt/storage/otte.rs.git
+GIT_WORK_TREE=/srv/http/otte.rs git checkout -f HEAD
 
 # I've set this up to install gems with bundler, and then start any rack app with a config.ru.
 cd /srv/http/otte.rs
