ca7b0213181370fd0a8896f13f2008c683b1b52c
Devyn Cairns <devyn.cairns@gmail.com>
2013-01-12 20:56:32 -0800

    Set umask

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 # Your assigned port range is 39410-39419, and you may use these for anything you want.
 # Your HTTP server should use port 39410. If you want to start more processes, just let me know.
 
+umask 007
+
 # I've set this up to install gems with bundler, and then start any rack app with a config.ru.
 cd /srv/http/otte.rs
 bundle install --deployment || exit 1
