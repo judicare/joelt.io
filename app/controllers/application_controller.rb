ce1709089bad795959ab169746051fc70dadf917
Joel <barebonesgraphics@gmail.com>
2013-01-14 19:16:10 -0500

    properly login from cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   
-  def current_uid
-    if cookies.signed[:user_id] && (u = User.find(cookies.signed[:user_id]))
-      session[:user_id] ||= u.id
+  before_filter :old_login
+  
+  def old_login
+    if !session[:user_id] && cookies.signed[:user_id] && (u = User.find(cookies.signed[:user_id]))
+      session[:user_id] = u.id
     end
-    session[:user_id]
   end
 end
