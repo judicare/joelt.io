499338fbb81ab1ec9b8a8e25bdef0d728d43227c
Joel <barebonesgraphics@gmail.com>
2013-01-22 17:14:03 -0500

    use github-style syntax highlighting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,20 +6,16 @@ module ApplicationHelper
 
   def syntax_highlighter(html)
     doc = Nokogiri::HTML(html)
-    doc.search("//pre[@lang]").each do |pre|
-      pre.replace("<figure>" + Pygments.highlight(pre.text.rstrip, lexer: pre[:lang], formatter: 'html', options: {encoding: 'utf-8', linenostart: pre[:number] || 1, linenos: 'table'}) + "</figure>")
+    doc.search("//pre/code").each do |code|
+      code.parent.replace("<figure>" + Pygments.highlight(code.text.rstrip, lexer: code[:class], formatter: 'html', options: {encoding: 'utf-8', linenostart: 1, linenos: 'table'}) + "</figure>")
     end
     doc.at_css("body").inner_html.to_s
   end
   
   def literate_haskell html
-    html.gsub(/^> /, "").strip.gsub(/<pre([^>]+)>(.*?)<\/pre>(?:\r\n)?/m) do
-      if $1.include?("haskell") && !$1.include?("nolhs")
-        $2.split("\n")[1..-1].map{|x|"> #{x}"}.join("\n")
-      else
-        "<pre>#{$2}</pre>\r\n"
-      end
-    end.gsub("&lt;", "<").gsub("&gt;", ">")
+    html.gsub(/^> /, "").strip.gsub(/``` haskell\r\n(.*?)```/m) do
+      $1.split("\r\n").map{|x|"> #{x}"}.join("\n")
+    end.gsub("&lt;", "<").gsub("&gt;", ">").gsub("\r", "")
   end
   
   def title t
