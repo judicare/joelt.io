8156c2fdb8f1969837750e0cc43fd28ab79f510f
Joel <barebonesgraphics@gmail.com>
2013-02-09 00:13:33 -0600

    forgot this was necessary

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,6 +53,10 @@ Otters::Application.configure do
   config.action_mailer.raise_delivery_errors = false
   
   config.action_mailer.delivery_method = :smtp
+    
+  config.action_mailer.smtp_settings = {
+    openssl_verify_mode: 'none'
+  }
 
   # Enable threaded mode
   # config.threadsafe!
