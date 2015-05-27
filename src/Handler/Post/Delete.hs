2f151c4eca9a144ecdd960bc5a1c451599705872
Jude Taylor <me@joelt.io>
2015-05-26 18:44:56 -0700

    messages

--- src/Handler/Post/Delete.hs
+++ src/Handler/Post/Delete.hs
@@ -5,5 +5,7 @@ import Import
 postDeletePostR :: PostId -> Handler Html
 postDeletePostR pid = do
     _ <- requireAuthId
+    post <- runDB $ get404 pid
     runDB $ delete pid
+    setMessageI $ MsgDeleted (postTitle post)
     redirect HomeR
