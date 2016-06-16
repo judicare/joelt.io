72c47e5e8ff647a0297cc6ab067e9077259bf8b6
Jude Taylor <me@jude.bio>
2016-06-16 13:18:31 -0700

    whoops

--- default.nix
+++ default.nix
@@ -1,26 +1,26 @@
 { mkDerivation, acid-state, base, blaze-html, blaze-markup
 , bytestring, cereal, clientsession, containers, cookie
-, digestive-functors, digestive-functors-blaze, directory
-, file-embed, haskeline, highlighter, http-types, ixset-typed
-, markdown, mtl, network, network-uri, pcre-light, pwstore-fast
-, safecopy, shakespeare, stdenv, template-haskell, text, time
-, transformers, utf8-string, vault, wai, wai-app-static, wai-extra
-, wai-session, wai-session-clientsession, warp, web-routes
-, web-routes-th
+, digestive-functors, digestive-functors-blaze, file-embed
+, highlighter, http-types, ixset-typed, markdown, mtl, network
+, network-uri, pcre-light, process-extras, pwstore-fast, safecopy
+, shakespeare, stdenv, template-haskell, text, time, transformers
+, vault, wai, wai-app-static, wai-extra, wai-session
+, wai-session-clientsession, warp, web-routes, web-routes-th
 }:
 mkDerivation {
   pname = "jude-web";
   version = "0.1.0.0";
   src = ./.;
+  configureFlags = [ "-fproduction" ];
   isLibrary = false;
   isExecutable = true;
   executableHaskellDepends = [
     acid-state base blaze-html blaze-markup bytestring cereal
     clientsession containers cookie digestive-functors
-    digestive-functors-blaze directory file-embed haskeline highlighter
-    http-types ixset-typed markdown mtl network network-uri pcre-light
-    pwstore-fast safecopy shakespeare template-haskell text time
-    transformers utf8-string vault wai wai-app-static wai-extra
+    digestive-functors-blaze file-embed highlighter http-types
+    ixset-typed markdown mtl network network-uri pcre-light
+    process-extras pwstore-fast safecopy shakespeare template-haskell
+    text time transformers vault wai wai-app-static wai-extra
     wai-session wai-session-clientsession warp web-routes web-routes-th
   ];
   license = stdenv.lib.licenses.mit;
