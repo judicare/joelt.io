cee0dec1c2c8e42284e896ba661792c4b84dfd22
Jude Taylor <me@jude.bio>
2016-10-16 16:24:36 -0700

    clean up nix files

--- default.nix
+++ default.nix
@@ -1,30 +1,32 @@
-{ mkDerivation, base, base64-bytestring, blaze-html, blaze-markup
-, bytestring, cereal, clientsession, containers, cookie, cryptohash
-, digestive-functors, digestive-functors-blaze, file-embed
-, highlighter, http-types, markdown, mime-types, monad-control
-, monad-logger, mtl, network, pcre-light, persistent
-, persistent-postgresql, persistent-template, process-extras
-, pwstore-fast, resource-pool, shakespeare, stdenv
-, template-haskell, text, time, transformers, vault, wai
-, wai-app-static, wai-extra, wai-session, wai-session-clientsession
-, warp, web-routes, web-routes-th
-}:
-mkDerivation {
-  pname = "jude-web";
-  version = "0.1.0.0";
-  src = ./.;
-  configureFlags = [ "-fproduction" ];
-  isLibrary = false;
-  isExecutable = true;
-  executableHaskellDepends = [
-    base base64-bytestring blaze-html blaze-markup bytestring cereal
-    clientsession containers cookie cryptohash digestive-functors
-    digestive-functors-blaze file-embed highlighter http-types markdown
-    mime-types monad-control monad-logger mtl network pcre-light
-    persistent persistent-postgresql persistent-template process-extras
-    pwstore-fast resource-pool shakespeare template-haskell text time
-    transformers vault wai wai-app-static wai-extra wai-session
-    wai-session-clientsession warp web-routes web-routes-th
-  ];
-  license = stdenv.lib.licenses.mit;
-}
+{ lib, haskell, buildBowerComponents, callPackage, writeTextDir, nodePackages, sass
+, compiler ? "ghc801" }:
+
+let
+  bowerPkgs = buildBowerComponents {
+    name = "jude.bio";
+    src = writeTextDir "bower.json" (builtins.readFile ./bower.json);
+    generated = ./generated/bower.nix;
+  };
+
+  nodePkgs = callPackage ./generated/node-composition.nix {};
+
+  pkg = haskell.packages."${compiler}".callPackage ./pkg.nix {};
+
+  inShell = lib.inNixShell;
+
+  releasePkg = haskell.lib.overrideCabal pkg (drv: rec {
+    buildTools = (drv.buildTools or [])
+      ++ [ sass nodePkgs.cssnano-cli ]
+      ++ lib.optional inShell nodePackages.bower;
+    doHaddock = false;
+    configureFlags = [ "-fproduction" ];
+    enableSharedExecutables = false;
+    enableSharedLibraries = false;
+    isLibrary = false;
+    preBuild = ''
+      ln -sfv ${bowerPkgs}/bower_components .
+    '';
+    shellHook = preBuild;
+  });
+
+in if inShell then releasePkg.env else releasePkg
