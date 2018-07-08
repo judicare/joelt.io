2a98fd1a00c51569484851157fe7a113d047e215
Jude Taylor <me@jude.xyz>
2018-07-07 20:28:02 -0700

    use node_modules

---
+++ Makefile
@@ -0,0 +1,22 @@
+OUT = dist
+STATIC_PREFIX = "https://jude.xyz/"
+FOUNDATION_SRC = .
+SRC = .
+
+all: $(OUT) $(OUT)/index.html $(OUT)/robots.txt $(OUT)/favicon.ico $(OUT)/style.css $(OUT)/img
+
+$(OUT):
+	mkdir -p $@
+
+$(OUT)/%: %
+	cp $< $@ --no-preserve=all
+
+$(OUT)/style.css: css/style.scss
+	(echo '$$static_prefix: $(STATIC_PREFIX);'; cat $<) | sass --scss -I $(FOUNDATION_SRC)/node_modules/foundation-sites/scss -I css --sourcemap=none /dev/stdin $(OUT)/style.css
+
+$(OUT)/img: $(wildcard img/*.png)
+	mkdir -p $@
+	$(foreach img,$^,optipng $(img) -out $(OUT)/img/$(notdir $(img));)
+
+clean:
+	rm -rf $(OUT)
--- default.nix
+++ default.nix
@@ -1,7 +1,17 @@
-{ runCommand }:
+{ runCommand, lib, sass, optipng, callPackage }:
 
-runCommand "jude-web" {} ''
-  mkdir -p $out
-  cp -R ${./.}/* $out
-  rm $out/*.nix
+let
+  cleanedSource = lib.cleanSourceWith {
+    filter = name: type: !(lib.elem (baseNameOf (toString name)) ["dist" "node_modules"]);
+    src = lib.sourceFilesBySuffices ./. [ "Makefile" ".png" ".ico" ".html" "robots.txt" ".scss" ];
+  };
+  foundation = (callPackage ./generated/node-composition.nix {}).foundation-sites;
+in
+
+runCommand "jude-web" {
+  buildInputs = [ sass optipng ];
+} ''
+  echo cd ${cleanedSource}
+  cd ${cleanedSource}
+  make OUT=$out FOUNDATION_SRC=${foundation}/lib
 ''
